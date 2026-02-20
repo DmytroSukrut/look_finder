9040fa9a7514bc667fdfffc09057fdb60dd1765f
Dmytro Sukrut <dmitriysukrut@gmail.com>
2026-02-20 22:16:40 +0100

    Trying to create server

--- src/main/java/com/look_finder/config/CorsConfig.java
+++ src/main/java/com/look_finder/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://lookfinder.xyz", "http://localhost")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
